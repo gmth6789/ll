301da41a0bdd41e37fa154e0ea8be47694fab40b
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2023-10-03 16:33:08 +0700

    1 file modified

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,40 +1,26 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Nuxt Starter',
-    htmlAttrs: {
-      lang: 'en'
-    },
+    title: 'Vant Nuxt Demo',
     meta: [
       { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { name: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, viewport-fit=cover' }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
+  plugins: ['~/plugins/vant'],
   build: {
+    babel: {
+      plugins: [
+        [
+          'import',
+          {
+            libraryName: 'vant',
+            // 目前在 nuxt 中无法按需引入样式，因此采用手动引入的方式
+            style: false
+          },
+          'vant'
+        ]
+      ]
+    }
   }
-}
+};
